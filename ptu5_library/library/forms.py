19c29c2b4bb90b1dd413e3febd8c742189701e4b
Kestutis Januskevicius <infohata@gmail.com>
2022-11-18 09:51:37 +0200

    fixing naive datetime comparison in forms

--- ptu5_library/library/forms.py
+++ ptu5_library/library/forms.py
@@ -9,7 +9,7 @@ class BookReviewForm(forms.ModelForm):
             reader = self.cleaned_data.get("reader")
             recent_posts = BookReview.objects.filter(
                 reader=reader, 
-                created_at__gte=(datetime.now()-timedelta(hours=1))
+                created_at__gte=(datetime.utcnow()-timedelta(hours=1))
             )
             if recent_posts:
                 return False
